4c64c1be128bbb98639bd4c8b3eee6a6f6a52a2c
eliannapot <up1072682@upnet.gr>
2025-07-23 17:04:07 +0300

    cleaned up code

--- scripts/streak-reset-function/src/main.js
+++ scripts/streak-reset-function/src/main.js
@@ -59,28 +59,4 @@ export default async ({ req, res, log, error }) => {
   }
 
 };
-    // const people = new Users(client);
-
-  // try {
-  //   const response = await people.list();
-  //   // Log messages and errors to the Appwrite Console
-  //   // These logs won't be seen by your end users
-  //   log(`Total users: ${response.total}`);
-  // } catch(err) {
-  //   error("Could not list users: " + err.message);
-  // }
-
-  // // The req object contains the request data
-  // if (req.path === "/ping") {
-  //   // Use res object to respond with text(), json(), or binary()
-  //   // Don't forget to return a response!
-  //   return res.text("Pong");
-  // }
-
-  // return res.json({
-  //   motto: "Build like a team of hundreds_",
-  //   learn: "https://appwrite.io/docs",
-  //   connect: "https://appwrite.io/discord",
-  //   getInspired: "https://builtwith.appwrite.io",
-  // });
 
